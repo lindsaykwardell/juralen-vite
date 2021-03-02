517c95bd99a873841d0cc274bf336110472989c3
Lindsay Wardell <three060@gmail.com>
2021-03-02 11:04:14 -0800

    Added pipe method

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,12 +1,13 @@
+export const pipe = (...fns: Array<(input: any) => any>) => <T>(data: T): any =>
+  fns.reduce((data, fn) => fn(data), data)
+
 export type Nothing = null | undefined
 export const nothing: Nothing = null
 
 export type Maybe<T> = T | Nothing
 export type Int = bigint
 export const Integer = {
-  abs(x: Int): Int {
-    return BigInt(Number(x))
-  },
+  abs: (x: Int): Int => pipe(Number, Math.round, BigInt)(x) as Int,
 }
 
 export enum GameStatus {
